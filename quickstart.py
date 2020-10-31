f8a148c57b454a11721279ee598f3c923f6dda07
TheDiplomats <socx@protonmail.com>
2020-10-31 16:50:46 -0700

    Split script into two versions depending on os

--- quickstart.py
+++ quickstart.py
@@ -1,35 +1,76 @@
 import os
 
+def runWin(username):
+    grabDjango = "git clone https://github.com/" + username + "/django.git"
 
-github_username = input("Please enter your github username: ")
+    print("Attempting to clone django project to your local machine")
+    gotDjango = os.system(grabDjango)
 
-grabDjango = "git clone https://github.com/" + github_username + "/django.git"
+    navigateToDjango = "cd django/"
+    print("Attempting to navigate to Django folder")
+    inDjango = os.system(navigateToDjango)
 
-print("Attempting to clone django project to your local machine")
-gotDjango = os.system(grabDjango)
+    print("Attempting to set virtual env")
+    setVirtEnv = "python3 -m venv ~/.virtualenvs/djangodev"
+    setEnv = os.system(setVirtEnv)
 
-navigateToDjango = "cd django/"
-print("Attempting to navigate to Django folder")
-inDjango = os.system(navigateToDjango)
+    print("Attempting to activate virtual env")
+    activateVirtEnv = "source ~/.virtualenvs/djangodev/bin/activate"
+    activatedEnv = os.system(activateVirtEnv)
 
-print("Attempting to set virtual env")
-setVirtEnv = "python3 -m venv ~/.virtualenvs/djangodev"
-setEnv = os.system(setVirtEnv)
+    cwd = os.getcwd()
 
-print("Attempting to activate virtual env")
-activateVirtEnv = "source ~/.virtualenvs/djangodev/bin/activate"
-activatedEnv = os.system(activateVirtEnv)
+    print("Attempting to install django and dependencies")
+    installRightHere = "python -m pip install -e" + cwd + "/django"
+    installed = os.system(installRightHere)
 
-cwd = os.getcwd()
+    grabDependencies = "python -m pip install -r requirements/py3.txt "
+    gotDependencies = os.system(grabDependencies)
 
-print("Attempting to install django and dependencies")
-installRightHere = "python -m pip install -e" + cwd + "/django"
-installed = os.system(installRightHere)
+    print("Attempting to run tests")
+    runTests = "./runtests.py"
+    ranTests = os.system(runTests)
 
-grabDependencies = "python -m pip install -r requirements/py3.txt "
-gotDependencies = os.system(grabDependencies)
 
-print("Attempting to run tests")
-runTests = "./runtests.py"
-ranTests = os.system(runTests)
+
+def runUni(username):
+    grabDjango = "git clone https://github.com/" + username + "/django.git"
+
+    print("Attempting to clone django project to your local machine")
+    gotDjango = os.system(grabDjango)
+
+    navigateToDjango = "cd django/"
+    print("Attempting to navigate to Django folder")
+    inDjango = os.system(navigateToDjango)
+
+    print("Attempting to set virtual env")
+    setVirtEnv = "python3 -m venv ~/.virtualenvs/djangodev"
+    setEnv = os.system(setVirtEnv)
+
+    print("Attempting to activate virtual env")
+    activateVirtEnv = "source ~/.virtualenvs/djangodev/bin/activate"
+    activatedEnv = os.system(activateVirtEnv)
+
+    cwd = os.getcwd()
+
+    print("Attempting to install django and dependencies")
+    installRightHere = "python -m pip install -e" + cwd + "/django"
+    installed = os.system(installRightHere)
+
+    grabDependencies = "python -m pip install -r requirements/py3.txt "
+    gotDependencies = os.system(grabDependencies)
+
+    print("Attempting to run tests")
+    runTests = "./runtests.py"
+    ranTests = os.system(runTests)
+
+
+
+def main():
+    github_username = input("please enter your github username: ")
+    if os.name == 'nt':
+        runWin(github_username)
+    else:
+        runUni(github_username)
+
 
