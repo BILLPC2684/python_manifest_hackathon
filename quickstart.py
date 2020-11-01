db502a3debc135cab120dd71f4f8b527223e0200
TheDiplomats <socx@protonmail.com>
2020-10-31 17:15:07 -0700

    Added windows implmentation to quickstart script

--- quickstart.py
+++ quickstart.py
@@ -1,43 +1,43 @@
 import os
 
-def runWin(username):
-    grabDjango = "git clone https://github.com/" + username + "/django.git"
+def runWin(djangoCommand):
+    print("Windows operating system detected")
 
     print("Attempting to clone django project to your local machine")
-    gotDjango = os.system(grabDjango)
+    gotDjango = os.system(djangoCommand)
 
     navigateToDjango = "cd django/"
     print("Attempting to navigate to Django folder")
     inDjango = os.system(navigateToDjango)
 
     print("Attempting to set virtual env")
-    setVirtEnv = "python3 -m venv ~/.virtualenvs/djangodev"
+    setVirtEnv = "py -m venv %HOMEPATH%\.virtualenvs\djangodev"
     setEnv = os.system(setVirtEnv)
 
     print("Attempting to activate virtual env")
-    activateVirtEnv = "source ~/.virtualenvs/djangodev/bin/activate"
+    activateVirtEnv = "%HOMEPATH%\.virtualenvs\djangodev\Scripts\activate.bat"
     activatedEnv = os.system(activateVirtEnv)
 
     cwd = os.getcwd()
 
     print("Attempting to install django and dependencies")
-    installRightHere = "python -m pip install -e" + cwd + "/django"
+    installRightHere = "py -m pip install -e " + cwd + "\django"
     installed = os.system(installRightHere)
 
-    grabDependencies = "python -m pip install -r requirements/py3.txt "
+    grabDependencies = "py -m pip install -r requirements\py3.txt"
     gotDependencies = os.system(grabDependencies)
 
     print("Attempting to run tests")
-    runTests = "./runtests.py"
+    runTests = "py runtests.py"
     ranTests = os.system(runTests)
 
 
 
-def runUni(username):
-    grabDjango = "git clone https://github.com/" + username + "/django.git"
+def runUni(djangoCommand):
+    print("Unix based operating system detected")
 
     print("Attempting to clone django project to your local machine")
-    gotDjango = os.system(grabDjango)
+    gotDjango = os.system(djangoCommand)
 
     navigateToDjango = "cd django/"
     print("Attempting to navigate to Django folder")
@@ -68,9 +68,12 @@ def runUni(username):
 
 def main():
     github_username = input("please enter your github username: ")
+    grabDjango = "git clone https://github.com/" + github_username + "/django.git"
     if os.name == 'nt':
-        runWin(github_username)
+        runWin(grabDjango)
     else:
-        runUni(github_username)
+        runUni(grabDjango)
 
 
+if __name__ == "__main__":
+    main()
